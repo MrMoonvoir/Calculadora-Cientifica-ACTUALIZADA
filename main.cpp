e05a351623ae2ce906eda383d514b70039cc2e24
MrMoonvoir <moonvoirblack03@gmail.com>
2025-11-26 20:35:08 -0600

    Update main.cpp
    actualizado 26/11/25

--- main.cpp
+++ main.cpp
@@ -1,54 +1,88 @@
 #include <iostream>
-#include <stdexcept>
+#include <cstdlib>
+#include <cmath>
+#include <thread>
+#include <chrono>
 #include "Calculadora.h"
 
 using namespace std;
 
+void menu(){
+
+    cout << "=== Calculadora Cientifica ===\n";
+    cout << "Menu:\n";
+    cout << "1. Suma. \n";
+    cout << "2. Resta. \n";
+    cout << "3. Multiplicación. \n";
+    cout << "4. División. \n";
+    cout << "5. Raíz cuadrada\n";
+    cout << "6. Potencia\n";
+    cout << "7. Logaritmo natural\n";
+    cout << "8. Salir \n";
+}
+
 int main() {
-    Calculadora calc;
+    Calculadora cientifica;
     int opcion;
     double a, b;
 
-    cout << "=== Calculadora Cientifica ===\n";
-
-    while (true) {
-        cout << "\nMenu:\n";
-        cout << "1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n";
-        cout << "Elige una opcion: ";
+    do {
+        menu();
+        cout << "Ingrese una opción: ";
         cin >> opcion;
 
-        if (opcion == 12) break;
-
-        try {
-            switch(opcion) {
-                case 1:
-                    cout << "Ingrese dos numeros: ";
-                    cin >> a >> b;
-                    cout << "Resultado: " << calc.suma(a,b) << "\n";
-                    break;
-                case 2:
-                    cout << "Ingrese dos numeros: ";
-                    cin >> a >> b;
-                    cout << "Resultado: " << calc.resta(a,b) << "\n";
-                    break;
-                case 3:
-                    cout << "Ingrese dos numeros: ";
-                    cin >> a >> b;
-                    cout << "Resultado: " << calc.multiplicacion(a,b) << "\n";
-                    break;
-                case 4:
-                    cout << "Ingrese dos numeros: ";
-                    cin >> a >> b;
-                    cout << "Resultado: " << calc.division(a,b) << "\n";
-                    break;
-                default:
-                    cout << "Opcion invalida.\n";
-            }
-        } catch (invalid_argument& e) {
-            cout << e.what() << "\n";
+        switch (opcion) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 6:
+                cout << "Ingrese dos números:\n";
+                cout << "a => ";
+                cin >> a;
+                cout << "b => ";
+                cin >> b;
+                break;
+            case 5:
+            case 7:
+                cout << "Ingrese un número:\n";
+                cout << "a => ";
+                cin >> a;
+                break;
         }
-    }
 
-    cout << "Gracias por usar la calculadora.\n";
+        switch (opcion) {
+            case 1:
+                cout << "Resultado: " << cientifica.suma(a, b) << "\n";
+                break;
+            case 2:
+                cout << "Resultado: " << cientifica.resta(a, b) << "\n";
+                break;
+            case 3:
+                cout << "Resultado: " << cientifica.multiplicacion(a, b) << "\n";
+                break;
+            case 4:
+                cout << "Resultado: " << cientifica.division(a, b) << "\n";
+                break;
+            case 5:
+                cout << "Resultado: " << cientifica.raizCuadrada(a) << "\n";
+                break;
+            case 6:
+                cout << "Resultado: " << cientifica.potencia(a, b) << "\n";
+                break;
+            case 7:
+                cout << "Resultado: " << cientifica.logaritmo(a) << "\n";
+                break;
+                
+            case 8:
+                cout << "Cerrando programa...\n";
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+                break;
+            default:
+                cout << "Opción inválida. Intente de nuevo.\n";
+        }
+
+    } while (opcion != 8);
+
     return 0;
 }
